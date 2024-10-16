7b20a661afcf118664f51e8d735996859f34b94b
Marukin-tnk <67319010014@tnk.ac.th>
2024-10-16 17:59:38 +0700

    test

--- test.py
+++ test.py
@@ -1,2 +1,18 @@
-print("Hello Python")
-print("my name is Marukin") 
+class Studen:
+    def __init__(self,name,scoure):
+        self.name = name 
+        self.scoure = scoure
+    def showgrade(self):
+        score = self.score
+        if score >= 50:
+            print(f"คุณ{self.name} ผ่านการสอบ")
+        else :
+            print(f"คุณ{self.name} ไม่ผ่านการสอบ")
+    def showscore(self):
+        print(f"คุณ {self.name} ได้คะแนน {self.score}")
+
+studen1 = Studen("สมชาย",70)
+studen2 = Studen("สมหญิง",49)        
+
+studen1.showgrade()
+studen2.showgrade()
